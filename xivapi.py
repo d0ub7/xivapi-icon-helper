d9c4fe06849ebdb3c97e80b4bcd483b16c493e44
Jason Deano <jmdeano@gmail.com>
2021-09-09 16:21:40 -0400

    get this submitted for potential rework

--- xivapi.py
+++ xivapi.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import argparse
+import math
 
 import pyxivapi
 from pyxivapi.models import Filter
@@ -10,23 +11,41 @@ API_KEY = ""
 
 URL_PREFIX = "https://xivapi.com/i/"
 async def fetch_example_results():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("name", help="name of skill to fetch")
-    args = parser.parse_args()
-
-    print(f"searching xivapi for {args.name}")
     client = pyxivapi.XIVAPIClient(api_key=API_KEY)
     action = await client.index_search(
-            name=args.name, 
+            name="*", 
             indexes=["Action"], 
             columns=["Name", "IconHD"], 
             filters=[
                 Filter("ClassJobLevel", "gte", 0)
             ],
-            string_algo="match"
+            string_algo="match",
+            per_page="1"
         )
-    print("\n====")
-    print(f"![{action['Results'][0]['Name']}]({action['Results'][0]['IconHD'].replace('/i/', URL_PREFIX)})")
+
+    pages = math.ceil(action["Pagination"]["ResultsTotal"]/100)
+    out_string = ""
+    for i in range(pages):
+        print(f'getting page {i+1}')
+        loop_action = await client.index_search(
+            name="*",
+            indexes=["Action"], 
+            columns=["Name", "IconHD"], 
+            filters=[
+                Filter("ClassJobLevel", "gte", 0)
+            ],
+            string_algo="match",
+            per_page="100",
+            page=i+1
+        )
+        for result in loop_action['Results']:
+           print(f"![{result['Name']}]({result['IconHD'].replace('/i/', URL_PREFIX)})")
+           out_string += "{"
+           out_string += f"\"{result['Name']}\": \"{result['IconHD'].replace('/i/', URL_PREFIX)}\""
+           out_string += "}"
+
+    with open("test.json", 'w+') as f:
+        f.write(out_string)
     await client.session.close()
 
 if __name__ == '__main__':
